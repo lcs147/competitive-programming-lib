444dc5661e32b919e58885947d8026c9c988ef6a
Lucas Castro <lcs147@hotmail.com>
2022-04-05 14:14:13 -0400

    update

--- header.cpp
+++ header.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define int long long
 using namespace std;
 
@@ -6,6 +6,8 @@ signed main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
     #define endl '\n'
+
+    
 
     return 0;
 }
