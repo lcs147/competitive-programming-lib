5ec4c5d801821988434fa1914d47a11909367e93
Lucas Castro <lcs147@hotmail.com>
2022-04-16 19:04:13 -0400

    update header

--- header.cpp
+++ header.cpp
@@ -4,19 +4,12 @@ using namespace std;
 template <class T> vector<T> create(size_t size, T initialValue) { return vector<T>(size, initialValue); }
 template <class T, class... Args> auto create(size_t head, Args&&... tail) { auto inner = create<T>(tail...); return vector<decltype(inner)>(head, inner); }
 
-void solve() {
-    
-}
-
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     #define endl '\n'
 
-    int t = 1;
-    // cin >> t;
-    while(t--)
-        solve();
+
 
     return 0;
 }
