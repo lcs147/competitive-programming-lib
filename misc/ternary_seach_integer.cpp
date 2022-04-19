5dee0be9c2add09ee5afc80df4952f349593d509
Lucas Castro <lcs147@hotmail.com>
2022-04-19 19:39:51 -0400

    add ternary search integer

--- misc/ternary_seach_integer.cpp
+++ misc/ternary_seach_integer.cpp
@@ -1,7 +1,7 @@
 int lo = 0, hi = 1e9 + 1;
 while(lo < hi) {
     int mid = (lo + hi) >> 1;
-    if(check(mid) < check(mid+1)) {
+    if(check(mid) < check(mid + 1)) {
         hi = mid;
     } else {
         lo = mid + 1;
