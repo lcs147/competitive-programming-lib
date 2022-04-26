390af180b4c9f392256a713eb4531d83f665c939
Lucas Castro <lcs147@hotmail.com>
2022-04-26 17:35:01 -0400

    update header

--- header.cpp
+++ header.cpp
@@ -3,6 +3,12 @@
 #define int long long
 using namespace std;
 
+#ifdef LOCAL
+    #include "../cp-lib/helpers/debug.cpp"
+#else
+    #define debug(...)
+#endif
+
 signed main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
