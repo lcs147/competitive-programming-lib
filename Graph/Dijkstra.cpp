4aad2a4d7a68d94e7ad8a7a71ff0b53e49c35981
Lucas Castro <lcs147@hotmail.com>
2024-09-24 18:31:28 -0400

    bla

---
+++ Graph/Dijkstra.cpp
@@ -0,0 +1,30 @@
+// const int mod = sqrt(numeric_limits<int>::max()) - 7;
+const int mod = 1e18 + 7;
+auto dijkstra = [](auto &g, int s) {
+    const int oo = 1e18, n = g.size();
+    vi d(n, oo), cnt(n, 0), mn(n, 0), mx(n, 0);
+
+    cnt[s] = 1;
+    set<pair<int, int>> q = {mp(d[s] = 0, s)};
+    while(sz(q)) {
+        auto [du, u] = *q.begin();
+        q.erase(q.begin());
+        for(auto &[v, w, id]: g[u]) {
+            if(du + w == d[v]) {
+                cnt[v] += cnt[u];
+                if(cnt[v] >= mod) cnt[v] -= mod;
+                ckmin(mn[v], mn[u] + 1);
+                ckmax(mx[v], mx[u] + 1);
+            }
+            if(du + w < d[v]) {
+                q.erase({d[v], v});
+                mn[v] = mn[u] + 1;
+                mx[v] = mx[u] + 1;
+                cnt[v] = cnt[u];
+                q.insert({d[v] = du + w, v});
+            }
+        }
+    }
+
+    return mt(d, cnt);
+};
